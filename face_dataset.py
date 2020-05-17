0bfaf112146d9b6b6802f57fb2f7c57d90296957
diyapeng <50388704+diyapeng@users.noreply.github.com>
2020-05-17 11:33:03 -0400

    Add files via upload

--- face_dataset.py
+++ face_dataset.py
@@ -32,7 +32,7 @@ def CatchPICFromVideo(window_name, camera_idx, catch_pic_num, path_name):
         if len(faceRects) > 0:                                            
             for faceRect in faceRects:  
                 x, y, w, h = faceRect                        
-               
+                
                 #img_name = "User." + str(face_id) + '.' + str(num) + ".jpg"               
                 image = frame[y - 10: y + h + 10, x - 10: x + w + 10]
                 cv2.imwrite("/home/pi/project/dataset/User." + str(face_id) + '.' + str(num) + ".jpg", image)                                
@@ -41,9 +41,7 @@ def CatchPICFromVideo(window_name, camera_idx, catch_pic_num, path_name):
                 if num > (catch_pic_num):  
                     break
                 
-                
                 cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), color, 2)
-                
                 
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 cv2.putText(frame,'num:%d' % (num),(x + 30, y + 30), font, 1, (255,0,255),4)                
@@ -51,12 +49,10 @@ def CatchPICFromVideo(window_name, camera_idx, catch_pic_num, path_name):
         
         if num > (catch_pic_num): break                
                        
-        
         cv2.imshow(window_name, frame)        
         c = cv2.waitKey(10)
         if c & 0xFF == ord('q'):
             break        
-    
     
     cap.release()
     cv2.destroyAllWindows() 
@@ -65,5 +61,4 @@ if __name__ == '__main__':
     if len(sys.argv) != 1:
         print("Usage:%s camera_id face_num_max path_name\r\n" % (sys.argv[0]))
     else:
-        CatchPICFromVideo("Intercepting face", 0, 100, '/home/pi/project/dataset')
-
+        CatchPICFromVideo("intercepting face", 0, 100, '/home/pi/project/dataset')
